57b85b17f4ce2ddbbdd6935c9e27751e2e66b2fb
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-12 20:46:59 -0500

    update

--- src/symbol/codec.py
+++ src/symbol/codec.py
@@ -1,16 +1,17 @@
 import treepoem
 import blake3
 
-def generar_nodo_svg(elemento_unicode, nombre_archivo):
+def generar_nodo_svg(unicode_id:int, nombre_archivo):
     # 1. Normalización y Hash (10 bytes para una matriz compacta)
     # Usamos BLAKE3 por su velocidad y seguridad
-    hash_bytes = blake3.blake3(elemento_unicode.encode('utf-8')).digest(length=10)
-    
+    #hash_bytes = blake3.blake3(elemento_unicode.encode('utf-8')).digest(length=10)
+    #4 bytes: El valor máximo es $2^{32} - 1 = \mathbf{4.294.967.295}$. 
+    datos_bytes = unicode_id.to_bytes(4, byteorder='big') 
     # 2. Generar el código de barras
     # Usamos treepoem para obtener la representación vectorial
     barcode = treepoem.generate_barcode(
         barcode_type='datamatrix',
-        data=hash_bytes,
+        data=datos_bytes,
         options={"parse": True, "square": True } # Permite manejar datos binarios correctamente
     )
     
--- src/test.py
+++ src/test.py
@@ -14,4 +14,4 @@ from src.symbol.codec import generar_nodo_svg
 
 
 
-generar_nodo_svg("carro", "test")
+generar_nodo_svg(5_748_434, "test")
