91406122720ac055c8feb0f57cd99b1ffa46d602
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-18 18:29:03 -0500

    update

--- train-desktop/app/components/TabTrainBlockControl.vue
+++ train-desktop/app/components/TabTrainBlockControl.vue
@@ -4,15 +4,18 @@
       <UButton
         v-for="tab in tabs"
         :key="tab"
+        class="tab"
         :class="{ tabActive: isTabActive(tab) }"
         @click="selectTab(tab)"
         color="neutral"
         :variant="getButtonVariant(tab)"
-        size="sm"
+        size="md"
         >{{ tab }}</UButton
       >
     </div>
-    <div>content</div>
+    <div>
+      <UTable :data="data" class="flex-1" />
+    </div>
   </div>
 </template>
 
@@ -30,6 +33,16 @@ const tabs = [
 ];
 const activeTab = ref("NOUN");
 
+const data = ref([
+  {
+    sign: "bank",
+    index: 132,
+    hash: "asxzdg"
+  },
+  { sign: "territory", index: 276, hash: "gdfgx" },
+   { sign: "area", index: 226, hash: "xsda" },
+]);
+
 function isTabActive(tabName: string) {
   return activeTab.value === tabName;
 }
@@ -46,17 +59,25 @@ function getButtonVariant(tabName: string) {
 <style lang="css" scoped>
 .container {
   height: 100%;
+  padding: 1rem;
   overflow: hidden;
+  caret-color: transparent;
   border-radius: var(--card-radius);
   background: var(--color-white);
   border: 1px solid var(--color-border);
 }
 
-.tabs{
+.tab {
+  font-weight: 400;
+  color: var(--color-text-secondary);
+}
+
+.tabs {
   border-bottom: 1px solid var(--color-border);
 }
 
 .tabActive {
+  color: var(--color-text);
   border-bottom: 1px solid var(--color-primary);
 }
 </style>
