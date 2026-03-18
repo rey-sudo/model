7bab3474435d11fbd6b8feeed99705bbc91559fb
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-18 10:52:44 -0500

    Update test.py

--- source/src/test.py
+++ source/src/test.py
@@ -7,9 +7,9 @@ nlp = spacy.load("en_core_web_sm")
 block = ("The West Bank is a landlocked territory near the Mediterranean coast of Western Asia, bordered by Jordan to the east and Israel to the north, south, and west. It is a region of immense historical and religious significance, housing ancient cities like Jericho, Hebron, and Bethlehem. Its landscape is characterized by rugged limestone hills and vast olive groves that have been cultivated for generations. Despite its cultural richness, the area is defined by a complex geopolitical reality and a fragmented administrative structure. This ongoing situation creates significant challenges for the daily lives and mobility of its residents, keeping the region at the center of international diplomatic focus.")
 prompt= "what is the west bank?"
 
-doc = nlp(prompt)
+doc = nlp(block)
 
-tags_interes = ["NOUN", "PROPN", "PRON", "VERB", "ADJ", "ADV", "ADP", "DET", "AUX"]
+tags_interes = ["NOUN", "PROPN", "VERB", "ADJ", "PRON", "ADV", "ADP", "DET", "AUX"]
 
 data_nlp = {
     tag: list(set(token.lemma_ for token in doc if token.pos_ == tag))
