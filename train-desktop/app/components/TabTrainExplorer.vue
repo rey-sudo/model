6032e14c1aca102d80b3a6e6bb08a02ee3fbcd64
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-18 20:52:35 -0500

    Update TabTrainExplorer.vue

--- train-desktop/app/components/TabTrainExplorer.vue
+++ train-desktop/app/components/TabTrainExplorer.vue
@@ -1,49 +1,100 @@
 <script setup lang="ts">
-import type { TreeItem } from '@nuxt/ui'
+import type { TreeItem } from "@nuxt/ui";
 
 const items = ref<TreeItem[]>([
   {
-    label: 'app/',
+    label: "dicts/",
     defaultExpanded: true,
     children: [
       {
-        label: 'composables/',
-        children: [
-          {
-            label: 'useAuth.ts',
-            icon: 'i-vscode-icons-file-type-typescript'
-          },
-          {
-            label: 'useUser.ts',
-            icon: 'i-vscode-icons-file-type-typescript'
-          }
-        ]
+        label: "unicode.json",
+        icon: "lucide-file-braces",
+      },
+    ],
+  },
+  {
+    label: "blocks/",
+    defaultExpanded: true,
+    children: [
+      {
+        label: "b_0_100.json",
+        icon: "lucide-file-braces",
       },
       {
-        label: 'components/',
-        defaultExpanded: true,
-        children: [
-          {
-            label: 'Card.vue',
-            icon: 'i-vscode-icons-file-type-vue'
-          },
-          {
-            label: 'Button.vue',
-            icon: 'i-vscode-icons-file-type-vue'
-          }
-        ]
-      }
-    ]
+        label: "b_1_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_2_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_3_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_4_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_5_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_6_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_7_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_8_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_9_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_10_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_11_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_12_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_13_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_14_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_15_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_16_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_17_100.json",
+        icon: "lucide-file-braces",
+      },
+      {
+        label: "b_18_100.json",
+        icon: "lucide-file-braces",
+      },
+    ],
   },
-  {
-    label: 'app.vue',
-    icon: 'i-vscode-icons-file-type-vue'
-  },
-  {
-    label: 'nuxt.config.ts',
-    icon: 'i-vscode-icons-file-type-nuxt'
-  }
-])
+]);
 </script>
 
 <template>
