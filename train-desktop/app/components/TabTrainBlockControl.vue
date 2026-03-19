5c24088d95d14c176edf2f61348a2ca834955e44
Juan Rey <sudo.delete.evil@gmail.com>
2026-03-18 19:08:26 -0500

    Update TabTrainBlockControl.vue

--- train-desktop/app/components/TabTrainBlockControl.vue
+++ train-desktop/app/components/TabTrainBlockControl.vue
@@ -13,13 +13,17 @@
         >{{ tab }}</UButton
       >
     </div>
+    <USeparator />
     <div>
-      <UTable :data="data" class="flex-1" />
+      <UTable :data="data" :columns="columns" class="flex-1" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { h, resolveComponent } from "vue";
+import type { TableColumn, TableRow } from "@nuxt/ui";
+
 const tabs = [
   "NOUN",
   "PROPN",
@@ -31,17 +35,71 @@ const tabs = [
   "DET",
   "AUX",
 ];
+
 const activeTab = ref("NOUN");
 
-const data = ref([
-  {
-    sign: "bank",
-    index: 132,
-    hash: "asxzdg"
-  },
-  { sign: "territory", index: 276, hash: "gdfgx" },
-   { sign: "area", index: 226, hash: "xsda" },
+const UBadge = resolveComponent("UBadge");
+
+type Payment = {
+  id: number;
+  sign: string;
+  status: string;
+};
+
+const data = ref<Payment[]>([
+  { id: 0, sign: "bank", status: "yes" },
+  { id: 1, sign: "territory", status: "yes" },
+  { id: 2, sign: "area", status: "not" },
 ]);
+
+const columns: TableColumn<Payment>[] = [
+  {
+    accessorKey: "id",
+    header: "#",
+    cell: ({ row }) => `${row.getValue("id")}`,
+  },
+  {
+    accessorKey: "sign",
+    header: "Sign",
+    cell: ({ row }) => {
+      return row.getValue("sign")
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Indexed",
+    meta: {
+      class: {
+        th: "text-right",
+        td: "text-right font-medium",
+      },
+    },
+
+    footer: ({ column }) => {
+      const total = column
+        .getFacetedRowModel()
+        .rows.reduce(
+          (acc: number, row: TableRow<Payment>) =>
+            acc + Number.parseFloat(row.getValue("id")),
+          0,
+        );
+
+      return `Total: 2 / ${total}`;
+    },
+
+    cell: ({ row }) => {
+      const color = {
+        yes: "success" as const,
+        not: "error" as const,
+        refunded: "neutral" as const,
+      }[row.getValue("status") as string];
+
+      return h(UBadge, { class: "capitalize", variant: "subtle", color }, () =>
+        row.getValue("status"),
+      );
+    },
+  },
+];
 
 function isTabActive(tabName: string) {
   return activeTab.value === tabName;
@@ -70,10 +128,6 @@ function getButtonVariant(tabName: string) {
 .tab {
   font-weight: 400;
   color: var(--color-text-secondary);
-}
-
-.tabs {
-  border-bottom: 1px solid var(--color-border);
 }
 
 .tabActive {
